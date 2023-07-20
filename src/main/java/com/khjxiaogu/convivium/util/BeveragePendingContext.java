830b362455b556eb43a3cd35d71acb3aeaa16e8b
khjxiaogu <54445543+khjxiaogu@users.noreply.github.com>
2023-07-20 17:31:20 +0800

    context

--- src/main/java/com/khjxiaogu/convivium/util/BeveragePendingContext.java
+++ src/main/java/com/khjxiaogu/convivium/util/BeveragePendingContext.java
@@ -1,7 +1,19 @@
 package com.khjxiaogu.convivium.util;
 
+import java.util.ArrayList;
+
 import com.teammoeg.caupona.data.recipes.IPendingContext;
+import com.teammoeg.caupona.util.FloatemStack;
+import com.teammoeg.caupona.util.FloatemTagStack;
 
 public class BeveragePendingContext extends IPendingContext {
+
+	public BeveragePendingContext(BeverageInfo info) {
+		items = new ArrayList<>(info.stacks.size());
+		for (FloatemStack fs : info.stacks) {
+			super.items.add(new FloatemTagStack(fs));
+			totalItems += fs.getCount();
+		}
+	}
 
 }

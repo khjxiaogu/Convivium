54a1bec28c7a00c665bb3c143300c732a1c4d670
khj xiaogu <54445543+khjxiaogu@users.noreply.github.com>
2023-07-25 00:07:54 +0800

    rebuild header

--- src/main/java/com/khjxiaogu/convivium/blocks/pestle_and_mortar/PamContainer.java
+++ src/main/java/com/khjxiaogu/convivium/blocks/pestle_and_mortar/PamContainer.java
@@ -1,22 +1,19 @@
 /*
- * Copyright (c) 2022 TeamMoeg
+ * Copyright (c) 2023 IEEM Trivium Society/khjxiaogu
  *
- * This file is part of Caupona.
+ * This file is part of Convivium.
  *
- * Caupona is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * Convivium is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE as published by
  * the Free Software Foundation, version 3.
  *
- * Caupona is distributed in the hope that it will be useful,
+ * Convivium is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU LESSER GENERAL PUBLIC LICENSE for more details.
  *
- * Specially, we allow this software to be used alongside with closed source software Minecraft(R) and Forge or other modloader.
- * Any mods or plugins can also use apis provided by forge or com.teammoeg.caupona.api without using GPL or open source.
- *
- * You should have received a copy of the GNU General Public License
- * along with Caupona. If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+ * along with Convivium. If not, see <https://www.gnu.org/licenses/>.
  */
 
 package com.khjxiaogu.convivium.blocks.pestle_and_mortar;

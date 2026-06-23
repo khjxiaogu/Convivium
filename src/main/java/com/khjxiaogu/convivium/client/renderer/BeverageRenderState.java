59ed5278608e0134f073b4afc6294200ff08e296
khjxiaogu <54445543+khjxiaogu@users.noreply.github.com>
2026-06-23 13:47:39 +0800

    use model

--- src/main/java/com/khjxiaogu/convivium/client/renderer/BeverageRenderState.java
+++ src/main/java/com/khjxiaogu/convivium/client/renderer/BeverageRenderState.java
@@ -1,10 +1,13 @@
 package com.khjxiaogu.convivium.client.renderer;
 
 
+import com.teammoeg.caupona.client.util.DynamicBlockModelReference;
+
 import net.minecraft.client.renderer.blockentity.state.BlockEntityRenderState;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 
 public class BeverageRenderState extends BlockEntityRenderState {
 	TextureAtlasSprite sprite;
 	int clr;
+	DynamicBlockModelReference model;
 }
